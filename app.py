80c57649b730ba46ddc1c9ed052dfdb1433bc6a2
asfriansah <125948935+asfriansah@users.noreply.github.com>
2023-05-08 22:13:35 +0700

    Update app.py
    change model

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ def main():
     gender = left.selectbox('Gender',('Male','Female'))
     travel = left.selectbox('Type Of Travel',('Personal Travel','Business Travel'))
     checkin = right.selectbox('Checkin Satisfaction',('0','1','2','3','4','5'))
-    age = right.number_input('Age')
-    flight = right.number_input('Flight Distance In KM')
-    arrival = right.number_input('Arrival Delay In Minutes')
+    age = right.number_input('Age',0,100)
+    flight = right.number_input('Flight Distance In KM',0,10000)
+    arrival = right.number_input('Arrival Delay In Minutes',0,5000)
     button = st.button('Predict')
 
     if button:
@@ -33,7 +33,7 @@ with st.sidebar:
     st.subheader('About')
     desc = """<div style="text-align: justify;">
                     This is an application for predicting whether airplane passengers are satisfied
-                    or dissatisfied. Predictions using the XGBoost algorithm model with an accuracy of 82%
+                    or dissatisfied. Predictions using the XGBoost algorithm model with an accuracy of 81,6%
               </div>"""
     st.markdown(desc, unsafe_allow_html=True)
 
@@ -47,10 +47,17 @@ with st.sidebar:
     with col3:
         st.write("")
     
-    title_alignment = """<h4 style = 'text-align: center'>created 2023 by andri asfriansah</h4>"""
+    title_alignment = """<h4 style = 'text-align: center'>created 2023 by andri asfriansah</h4>
+                         <h4 style = 'text-align: center'>DASI (Drajatul Khair-Andri Asfriansah-
+                         Safira Khairunnisa-Inggil Bagas)</h4>"""
     st.markdown(title_alignment, unsafe_allow_html=True)
 
-with open('model/model2804.pkl','rb') as file:
+    st.image('model/dslogo.png')
+
+with open('model/scaler.pkl','rb') as file:
+    scal = pickle.load(file)
+
+with open('model/xgboost1.pkl','rb') as file:
     model = pickle.load(file)
 
 def predict(customer,clas,gender,age,travel,flight,checkin,arrival):
@@ -60,47 +67,12 @@ def predict(customer,clas,gender,age,travel,flight,checkin,arrival):
     cls = 0 if clas == 'Economy' else 1 if clas == 'Economy Plus' else 2
     sex = 0 if gender == 'Male' else 1
     trav = 0 if travel == 'Personal Travel' else 1
-    
-    if age <= 10 :
-        newage = 0
-    elif age > 11 and age <= 20 :
-        newage = 1
-    elif age > 21 and age <= 30 :
-        newage = 2
-    elif age > 31 and age <= 40 :
-        newage = 3
-    elif age > 41 and age <= 50 :
-        newage = 4
-    elif age > 51 and age <= 60 :
-        newage = 5
-    elif age > 61 and age <= 70 :
-        newage = 6
-    else:
-        newage = 7
-   
-    if arrival <= 180 :
-        newarrival = 0
-    elif arrival > 180 and arrival <= 360 :
-        newarrival = 1
-    elif arrival > 360 and arrival <= 540 :
-        newarrival = 2
-    elif arrival > 540 and arrival <= 720 :
-        newarrival = 3
-    elif arrival > 720 and arrival <= 900 :
-        newarrival = 4
-    elif arrival> 900 and arrival <= 1080 :
-        newarrival = 5
-    elif arrival > 1080 and arrival <= 1260 :
-        newarrival = 6
-    elif arrival > 1260 and arrival <= 1440 :
-        newarrival= 7
-    else:
-        newarrival = 8
-    
-    predictor = np.array([cust,cls,sex,newage,trav,flight,checkin,newarrival])
+        
+    predictor = np.array([cust,cls,sex,age,trav,flight,checkin,arrival])
     predictor1 = predictor.reshape(1, -1) 
     predictor1 = predictor1.astype(int)
-    prediction = model.predict(predictor1)
+    predictor_scal = scal.transform(predictor1)
+    prediction = model.predict(predictor_scal)
     verdict = 'Dissatisfied' if prediction == 0 else 'Satisfied'
     return verdict
 
